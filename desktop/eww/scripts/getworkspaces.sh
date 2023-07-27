78324c70557dba5dbfc185a4b34f4d65e45f4021
fullzer4 <gabrielpelizzaro@gmail.com>
2023-07-27 00:24:42 -0300

    workspaces

--- desktop/eww/scripts/getworkspaces.sh
+++ desktop/eww/scripts/getworkspaces.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-json_data=$(hyprctl workspaces -j)
+monitor_name="$1"
 
-create_monitor_json() {
-    monitor_name=$1
-    monitor_json=$(echo "$json_data" | jq --arg m "$monitor_name" 'map(select(.monitor == $m) | {id: .id})')
+spaces() {
+    json_data=$(hyprctl workspaces -j)
+    monitor_json=$(echo "$json_data" | jq --arg m "$monitor_name" 'map(select(.monitor == $m) | {id: .id|tostring})')
+    echo $monitor_json
 }
 
-if [ -z "$1" ]; then
-    all_json=$(echo "$json_data" | jq '{id: .id, monitor: .monitor}')
-else
-    monitor_name="$1"
-    create_monitor_json "$monitor_name"
-fi
+monitor_spaces() {
+    socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
+        spaces
+    done
+}
 
+spaces
+monitor_spaces
