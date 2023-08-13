7925cf92b8b9f61b5db2449f682975419b04c7ea
fullzer4 <gabrielpelizzaro@gmail.com>
2023-08-13 18:41:14 -0300

    desktop

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -5,11 +5,13 @@ let
 in
 
 {
-  imports =
-    [ 
-      ./hardware-configuration.nix
-      (import "${home-manager}/nixos")
-    ];
+  imports = [
+    ./hardware-configuration.nix
+    (import "${home-manager}/nixos")
+    ./nvidia/nvidia.nix
+    ./desktop/hyprland.nix
+    ./pkgs.nix
+  ];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -52,59 +54,6 @@ in
 
   services.getty.autologinUser = "fullzer4";
 
-  nixpkgs.config.allowUnfree = true;
-
-  environment.systemPackages = with pkgs; [
-     hyprland
-     kitty
-     wofi
-     eww
-     pavucontrol
-     dunst
-     libnotify
-     
-
-     firefox
-     chromium
-     obsidian
-     discord
-
-     vim
-     wget
-
-     git
-     python312
-     nodejs_20
-     deno
-     rustc
-     rustup
-     rustfmt
-     gnat13
-     gccgo13
-     clang-tools
-     gnumake
-     cmake
-     ninja
-     gdb
-     clang-tools
-     cppcheck
-
-     docker
-     docker-compose
-     podman
-     podman-compose
-  ];
-
-  sound.enable = true;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
-
   programs.mtr.enable = true;
   programs.gnupg.agent = {
     enable = true;
@@ -122,62 +71,15 @@ in
   # networking.firewall.allowedUDPPorts = [ ... ];
   networking.firewall.enable = false;
 
-  programs.hyprland = {
-    enable = true;
-    nvidiaPatches = true;
-    xwayland.enable = true;
-  };
-  
-  environment.sessionVariables = {
-    WLR_NO_HARDWARE_CURSORS = "1";
-    NIXOS_OZONE_WL = "1";
-  };
-
-  hardware = {
-    opengl = {
-      enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
-    };
-    nvidia = {
-      modesetting.enable = true;
-      open = false;
-      nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
-    };
-  };
-
-  services.xserver.videoDrivers = ["nvidia"];
-
   home-manager.users.fullzer4 = { config, pkgs, ... } : {
     home.stateVersion = "23.05";
 
-    #imports = [
-    #  "/etc/nixos/code/git.nix"
-    #];
-
-    home.file.".ssh/allowed_signers".text =
-    "* ${builtins.readFile /home/fullzer4/.ssh/id_rsa.pub}";
-
-    programs.bash.enable = true;
-    programs.zsh.enable = true;
-    programs.git = {
-      enable = true;
-      userName = "fullzer4";
-      userEmail = "gabrielpelizzaro@gmail.com";
-      extraConfig = {
-        commit.gpgsign = true;
-        gpg.format = "ssh";
-        gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
-        user.signingkey = "~/.ssh/id_rsa.pub";
-      };
-    };
+    imports = [
+      ./code/git.nix
+      ./terminal/zsh.nix
+      ./terminal/bash.nix
+    ];
     
-    programs.bash.initExtra = ''
-      if [ "$(tty)" = "/dev/tty1" ];then
-        exec Hyprland
-      fi
-    '';
   };  
 
   system.stateVersion = "23.05";
---
+++ desktop/nvidia/nvidia.nix
@@ -0,0 +1,19 @@
+{config, ...}:
+
+{
+  hardware = {
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+    nvidia = {
+      modesetting.enable = true;
+      open = false;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+  };
+
+  services.xserver.videoDrivers = ["nvidia"];
+}
